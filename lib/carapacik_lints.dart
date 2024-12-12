24535a110116e3083fe44e3fe8630d3e8da79cbe
Roman Laptev <carapacik@gmail.com>
2024-12-12 10:59:12 +0300

    1.10.0 (#11)
    * Update minimum Dart SDK version to 3.6
    * Add lints:
      * [`avoid_futureor_void`](https://dart.dev/tools/linter-rules/avoid_futureor_void)
      * [`always_use_package_imports`](https://dart.dev/tools/linter-rules/always_use_package_imports)
      * [`use_truncating_division`](https://dart.dev/tools/linter-rules/use_truncating_division)
    * Remove lints:
      * [`avoid_null_checks_in_equality_operators`](https://dart.dev/tools/linter-rules/avoid_null_checks_in_equality_operators)
      * [`package_api_docs`](https://dart.dev/tools/linter-rules/package_api_docs)

--- lib/carapacik_lints.dart
+++ lib/carapacik_lints.dart
@@ -4,7 +4,7 @@
 /// 1. Add it to your dev_dependencies
 /// ```yaml
 /// dev_dependencies:
-///   carapacik_lints: ^1.9.2
+///   carapacik_lints: ^1.10.0
 /// ```
 ///
 /// 2. Include the rules into your `analysis_options.yaml`
